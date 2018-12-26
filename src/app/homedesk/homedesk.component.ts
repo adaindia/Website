753c1a3c172c0ca9be21307e0062a8a8df814f88
adaindia <krishna@adaindia.in>
2018-12-26 12:31:25 +0530

    after gsap

--- src/app/homedesk/homedesk.component.ts
+++ src/app/homedesk/homedesk.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+
+
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Circ, Back, Elastic, Power1, Bounce,Power4} from 'gsap/all';
+
+
+
+declare var TweenMax: any;
+declare var TimelineMax: any;
+declare var $: any;
 
 @Component({
   selector: 'app-homedesk',
@@ -6,11 +15,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homedesk.component.scss']
 })
 export class HomedeskComponent implements OnInit {
+  @ViewChild('pikachu') box: ElementRef;
 
-  constructor() { }
+  ngOnInit(){
 
-  ngOnInit() {
-  }
 
-}
+//to() method
+let toElement = document.querySelector('#to-element');
+
+
+
+
+toElement.addEventListener('mouseenter', () => {
+  TweenMax.to(toElement, 1, { rotation: 360 });
+});
+
+
+// cache elements in variables
+const box1 = document.querySelector('.box1'),
+      box2 = document.querySelector('.box2'),
+      topText = document.querySelector('.top-text'),
+      middleText = document.querySelector('.middle-text'),
+      bottomText = document.querySelector('.bottom-text'),
+      replayBtn = document.querySelector('.controls__replay-btn'),
+      tl = new TimelineMax();
+
+// set initial property values before animation
+tl.set([topText, middleText, bottomText], {
+  scale: 0.1,
+  autoAlpha: 0
+})
+.set(topText, {y: '-=300'})
+.set(middleText, {x: '+=1000'})
+.set(bottomText, {y: '+=1000'})
+.to(box1, 1, {  // box 1 rotation
+  rotation: 45,
+  transformOrigin: 'center bottom',
+  ease: Elastic.easeOut
+})
+.to(box2, 1, { // box 2 rotation
+  rotation: -45,
+  transformOrigin: 'center bottom',
+  ease: Elastic.easeOut
+}, '-=1')
+.to(box1, 1, {
+  rotation: -360,
+  ease: Circ.easeIn
+})
+.to(box2, 1, {
+  rotation: 360,
+  ease: Circ.easeIn
+}, '-=1')
+.to(box1, 1, {
+  rotation: 45
+})
+.to(box2, 1, {
+  rotation: -45
+}, '-=1')
+.to([box1,box2], 1, { // both boxes scale down and disappear
+  rotationX: 180,
+  scale: 0
+})
+.to(topText, 0.5, { // text animation begins
+  scale: 1,
+  autoAlpha: 1,
+  rotation: 360,
+  y: 0
+}, '-=0.5')
+.to(middleText, 0.5, {
+  scale: 1,
+  autoAlpha: 1,
+  rotation: -360,
+  x: 0
+}, '-=0.3')
+.to(bottomText, 0.5, {
+  scale: 1,
+  autoAlpha: 1,
+  y: 0
+}, '-=0.3')
+.to(topText, 0.8, {
+  y: '-=40',
+  ease: Back.easeOut.config(4)
+}, '+=0.4')
+.to(middleText, 0.6, {
+  y: '+=21',
+  x: '+=20',
+  ease: Back.easeOut.config(4)
+}, '-=0.6');
+
+// replay
+replayBtn.addEventListener('click', function() {
+  tl.restart();
+});
+
+
+
+}}
 
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
 
+
+
+
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Circ, Back, Elastic, Power4} from 'gsap/all';
+
+declare var TweenMax: any;
+declare var TimelineMax: any;
+declare var $: any;
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  @ViewChild('') box: ElementRef;
 
-  constructor() { }
+  ngOnInit(){
 
-  ngOnInit() {
-  }
 
-}
+
+//vars
+const alice = document.querySelectorAll('.example__element');
+
+//elements start from a transform rotate of 0 degrees 
+//and end taking up the default position as set 
+//in CSS. repeat set to -1 makes the animation 
+//last indefinitely.
+//yoyo set to true makes the animation alternate
+//repeatDelay puts a 1 second delay in between alternations
+TweenMax.from(alice, 1.5, {
+  rotation: 0,
+  delay: 1.5,
+  repeat: -1,
+  repeatDelay: 1,
+  yoyo: true,
+  ease:  Power4.easeIn
+});
+
+
+}}
 
